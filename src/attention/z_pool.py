328ce71f2c9f80223dadff8860de4a3a63a0e5fe
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-01-31 00:49:19 +0300

    Create z_pool.py

---
+++ src/attention/z_pool.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+
+
+class ZPool(nn.Module):
+    def forward(self, x):
+        max_pool = torch.max(x, dim=1, keepdim=True)[0]
+        avg_pool = torch.mean(x, dim=1, keepdim=True)
+        return torch.cat([max_pool, avg_pool], dim=1)
