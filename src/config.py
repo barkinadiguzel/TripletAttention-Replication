2a2c56cc3dfa2075970f94f006bb05dbb970e1ff
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-01-31 00:52:37 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,13 @@
+class Config:
+    # Attention
+    KERNEL_SIZE = 7         
+    USE_SPATIAL = True      
+    USE_CHANNEL = True
+
+    # Model
+    IN_CHANNELS = 3
+    BASE_CHANNELS = 64
+    NUM_BLOCKS = 3           
+
+    # Visualization
+    SAVE_ATT_MAPS = True
